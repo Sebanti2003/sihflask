1213996290e8e86201682a2c87f0ce70f35e6921
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-09-06 00:16:11 +0530

    done

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
+from waitress import serve
 from app import app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Instead of app.run(), use Waitress to serve the app
+    serve(app, host="0.0.0.0", port=8080) 
